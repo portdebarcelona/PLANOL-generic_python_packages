57fb2c0657425c1947e88d9acaeb2fb74652df2e
Ernesto Arredondo Martinez <ernestone@gmail.com>
2020-03-29 01:14:52 +0100

    Correct script_ddl_to_scd.cmd to use installed pckg

--- extra_utils_pckg/extra_utils/ddl_to_scd/run_scripts/script_ddl_to_scd.cmd
+++ extra_utils_pckg/extra_utils/ddl_to_scd/run_scripts/script_ddl_to_scd.cmd
@@ -4,4 +4,4 @@ rem * Lanza proceso para convertir DDL de tabla Oracle en forma versionada tipo 
 rem * Los parametros que se pueden pasar se pueden averiguar con "-- --help"
 rem ***************************************************************************************
 
-%~dp0..\..\run_py3.bat -m ddl_to_scd.ddl_to_scd %*
+python -m extra_utils.ddl_to_scd.ddl_to_scd %*
