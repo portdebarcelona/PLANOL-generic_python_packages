22b5d2e11767b494d8952506d88981c37e2c4320
arredoer <ernesto.arredondo@portdebarcelona.cat>
2023-03-10 19:34:01 +0100

    fix problem with new version instant_oracle_client

--- config/docker/docker_build.cmd
+++ config/docker/docker_build.cmd
@@ -1,10 +1,10 @@
 pushd "%~dp0../.."
 
 rem Docker build VENV image
-docker build -t ernestone/python_gdal_oracle_geopandas -t planolport/python_gdal_oracle_geopandas .
+docker build -t ernestone/python_gdal_oracle_geopandas:latest -t planolport/python_gdal_oracle_geopandas:training .
 
 rem Docker build CONDA image
-docker build -f Dockerfile.conda -t ernestone/conda_gdal_oracle_geopandas -t planolport/conda_gdal_oracle_geopandas .
+docker build -f Dockerfile.conda -t ernestone/conda_gdal_oracle_geopandas:latest -t planolport/conda_gdal_oracle_geopandas:training .
 
 rem Docker compose build
 docker compose build python_packages
