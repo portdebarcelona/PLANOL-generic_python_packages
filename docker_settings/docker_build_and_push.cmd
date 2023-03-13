fbaa786db879b71594b70fd14a1f570222d95f11
arredoer <ernesto.arredondo@portdebarcelona.cat>
2023-03-13 12:38:56 +0100

    chgs build scripts to use tag training

--- docker_settings/docker_build_and_push.cmd
+++ docker_settings/docker_build_and_push.cmd
@@ -1,6 +1,6 @@
-set URL_REPO_GITHUB=https://github.com/portdebarcelona/PLANOL-generic_python_packages.git#master
+set URL_REPO_GITHUB=https://github.com/portdebarcelona/PLANOL-generic_python_packages.git#training
 
-set TAG_IMAGE=planolport/python_gdal_oracle_geopandas:latest
+set TAG_IMAGE=planolport/python_gdal_oracle_geopandas:training
 
 docker build --tag %TAG_IMAGE% %URL_REPO_GITHUB%
 
