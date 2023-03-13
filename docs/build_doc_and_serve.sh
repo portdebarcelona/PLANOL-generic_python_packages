8cae7f26fdb7b8b991fa4388fd704ba6741747fb
arredoer <ernesto.arredondo@portdebarcelona.cat>
2023-03-13 13:29:15 +0100

    unignore logo & use link logo on doc deployed

--- docs/build_doc_and_serve.sh
+++ docs/build_doc_and_serve.sh
@@ -3,8 +3,9 @@ cd "$(dirname "${0}")" || exit
 pip install pdoc
 
 PACKAGES_DOC=${PACKAGES_DOC:-cx_oracle_spatial extra_osgeo_utils extra_utils pandas_utils spatial_utils}
+URL_LOGO_APB=${URL_LOGO_APB:-http://planoldev.portdebarcelona.cat:8022/sftp/api/public/dl/cq4LBWhn?inline=true}
 
-pdoc $PACKAGES_DOC -t ./templates -d google --logo ./logo.png -o ./html
+pdoc $PACKAGES_DOC -t ./templates -d google --logo ${URL_LOGO_APB} -o ./html
 
 cd ./html || exit
 
