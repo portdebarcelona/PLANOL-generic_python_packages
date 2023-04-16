37776c1aaa5420c656df895cc7ec0611af94f268
arredoer <ernesto.arredondo@portdebarcelona.cat>
2023-04-16 20:55:12 +0200

    refactor docs to use for different templates dirs

--- docs/build_doc_and_serve.sh
+++ docs/build_doc_and_serve.sh
@@ -4,8 +4,10 @@ pip install pdoc
 
 PACKAGES_DOC=${PACKAGES_DOC:-cx_oracle_spatial extra_osgeo_utils extra_utils pandas_utils spatial_utils}
 URL_LOGO_APB=${URL_LOGO_APB:-http://planoldev.portdebarcelona.cat:8022/sftp/api/public/dl/cq4LBWhn?inline=true}
+MAIN_README_DOC=${MAIN_README_DOC:-README.md}
+JINJA_TEMPLATES=${JINJA_TEMPLATES:-./templates}
 
-pdoc $PACKAGES_DOC -t ./templates -d google --logo ${URL_LOGO_APB} -o ./html
+pdoc $PACKAGES_DOC -t "${JINJA_TEMPLATES}" -d google --logo ${URL_LOGO_APB} -o ./html
 
 cd ./html || exit
 
