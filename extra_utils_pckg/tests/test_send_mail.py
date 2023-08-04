3b205f2fb7dcfef971959b5bb0c417041e02ae66
Àngela Feliu <afeliu@nexusgeographics.com>
2023-08-04 11:04:17 +0200

    Variables d'entorn per api key

--- extra_utils_pckg/tests/test_send_mail.py
+++ extra_utils_pckg/tests/test_send_mail.py
@@ -1,11 +1,13 @@
 import unittest
 
 from extra_utils.send_mail import enviar_mail, send_grid
+import os
 
 
 class MyTestMails(unittest.TestCase):
     def setUp(self) -> None:
         self.mail = 'ernesto.arredondo@portdebarcelona.cat'
+        self.api_key = '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' #os.getenv('SEND_GRID_API_KEY')
 
     def test_enviar_mail(self):
         codi = enviar_mail('Test', 'This is a test', [self.mail])
@@ -14,7 +16,7 @@ class MyTestMails(unittest.TestCase):
     def test_sendgrid(self):
         result = send_grid(subject='prova',
                            body='prova',
-                           api_key='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',
+                           api_key=self.api_key,
                            user_mail_list=['afeliu@nexusgeographics.com'])
         self.assertIsTrue(result)
 
