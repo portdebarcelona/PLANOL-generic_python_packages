6ab3235e0afacb1c74e2b1e1f131cc01cd8064ea
arredoer <ernesto.arredondo@portdebarcelona.cat>
2023-12-11 15:33:12 +0100

    minor upgrades docker cmd scripts

--- docker_settings/docker_build_local.cmd
+++ docker_settings/docker_build_local.cmd
@@ -1,5 +1,7 @@
 pushd %~dp0..
 
+call docker_settings/create_volumes.cmd
+
 set TAG_IMAGE=planolport/python_gdal_oracle_geopandas:training
 
 docker build --tag %TAG_IMAGE% --file Dockerfile %CD%
--- docker_settings/docker_build_mamba_local.cmd
+++ docker_settings/docker_build_mamba_local.cmd
@@ -1,5 +1,7 @@
 pushd %~dp0..
 
+call docker_settings/create_volumes.cmd
+
 set TAG_IMAGE=planolport/mamba_gdal_oracle_geopandas:training
 
 docker build --tag %TAG_IMAGE% --file Dockerfile.mamba %CD%
