43b1de0b3ccb06d4e5c84ca84d79f685bdb4dc60
arredoer <ernesto.arredondo@portdebarcelona.cat>
2023-12-15 16:51:07 +0100

    remove docker files based on conda

--- docker_settings/docker_build_conda_local.cmd
+++
@@ -1,9 +0,0 @@
-pushd %~dp0..
-
-call docker_settings/create_volumes.cmd
-
-set TAG_IMAGE=planolport/conda_gdal_oracle_geopandas:training
-
-docker build --tag %TAG_IMAGE% --file Dockerfile.conda %CD%
-
-popd
