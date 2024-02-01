c5b2ee3e6d280e64ab346303e7986c46b02df884
arredoer <ernesto.arredondo@portdebarcelona.cat>
2024-02-01 10:13:47 +0100

    extra_utils: no root_logger con level DEBUG by default + add get_base_logger - pro: evitar que loggers de librerías requeridas printen DEBUG msgs - con: para tener control de cada logger, evitar usar el root_logger y usar nuevo get_base_logger para el que habrá que controlar el level por separado

--- extra_utils_pckg/tests/test_utils_logging.py
+++ extra_utils_pckg/tests/test_utils_logging.py
@@ -1,8 +1,9 @@
 import os
 import unittest
-from logging import DEBUG, ERROR
+from logging import DEBUG, ERROR, INFO
 
-from extra_utils.utils_logging import get_root_logger, get_file_logger, logger_path_logs
+from extra_utils import misc
+from extra_utils.utils_logging import get_root_logger, get_file_logger, get_base_logger, logger_path_logs
 
 
 class TestUtilsLogging(unittest.TestCase):
@@ -15,8 +16,29 @@ class TestUtilsLogging(unittest.TestCase):
         logger = get_root_logger()
         self.assertIsNotNone(logger)
 
+    def test_base_logger(self):
+        """ Create a test for function get_base_logger"""
+        get_root_logger().setLevel(DEBUG)
+        logger = get_base_logger(level=INFO)
+        self.assertEqual(misc.caller_name(0), logger.name)
+        logger.debug("debug")
+        logger.info("info")
+        logger.warning("warning")
+        logger.error("error")
+        logger.critical("critical")
+        nom_base_log = 'test_base_logger'
+
+        logger2 = get_base_logger(nom_base_log, level=DEBUG)
+        self.assertEqual(nom_base_log, logger2.name)
+        logger2.debug("debug")
+        logger2.info("info")
+        logger2.warning("warning")
+        logger2.error("error")
+        logger2.critical("critical")
+        self.assertTrue(logger2.hasHandlers())
+
     def test_file_logger(self):
-        logger = get_file_logger('test_file_logger', DEBUG, self.path_logs)
+        logger = get_file_logger('test_file_logger', dir_log=self.path_logs)
         logger.debug("debug")
         logger.info("info")
         logger.warning("warning")
@@ -24,7 +46,7 @@ class TestUtilsLogging(unittest.TestCase):
         logger.critical("critical")
         self.assertEqual(len(logger_path_logs(logger)), 2)  # REPORT AND PROBLEMS
 
-        logger = get_file_logger('test_file_logger_2', DEBUG, self.path_logs, separate_reports=False)
+        logger = get_file_logger('test_file_logger_2', INFO, self.path_logs, separate_reports=False)
         logger.debug("debug")
         logger.info("info")
         logger.warning("warning")
