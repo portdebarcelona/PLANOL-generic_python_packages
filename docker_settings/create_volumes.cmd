b553f7dd5c27c274e8419ad46752689ccf0dbcd9
arredoer <ernesto.arredondo@portdebarcelona.cat>
2024-04-21 15:13:03 +0200

    use docker volumes & ports specifics for test the repo + use docker image postgis

--- docker_settings/create_volumes.cmd
+++ docker_settings/create_volumes.cmd
@@ -1,2 +1,2 @@
-docker volume create --name db_postgis_vol -d local
-docker volume create --name oracle_xe-vol -d local
+docker volume create --name db_postgis_vol-generic_python -d local
+docker volume create --name oracle_xe_vol-generic_python -d local
