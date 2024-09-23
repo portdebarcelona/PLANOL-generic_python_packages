076b12c3935d0f25cbc2f7cee2088f0149d13045
arredoer <ernesto.arredondo@portdebarcelona.cat>
2024-09-23 16:21:06 +0200

    geopandas_utils: fix gdf_from_df -> only setting index when idx has column names

--- pandas_utils_pckg/pandas_utils/geopandas_utils.py
+++ pandas_utils_pckg/pandas_utils/geopandas_utils.py
@@ -118,7 +118,10 @@ def gdf_from_df(df: DataFrame, geom_col: str, crs: str, cols_geom: list[str] = N
 
     df_aux = df.copy()
     idx_prev = df_aux.index
-    df_aux.reset_index(inplace=True)
+    # We only deal with index when has names setted referred to possible columns
+    set_idx = None not in idx_prev.names
+    if set_idx:
+        df_aux.reset_index(inplace=True)
 
     for col in (col for col in df_aux.columns if col in cols_geom):
         if (dtype := df_aux[col].dtype.name) == 'object':
@@ -126,7 +129,8 @@ def gdf_from_df(df: DataFrame, geom_col: str, crs: str, cols_geom: list[str] = N
         elif dtype == 'geometry':
             df_aux[col] = GeoSeries(df_aux[col].tolist(), crs=crs)
 
-    df_aux = df_aux.set_index(idx_prev, drop=True)
+    if set_idx:
+        df_aux = df_aux.set_index(idx_prev.names, drop=True)
 
     gdf = GeoDataFrame(df_aux, geometry=geom_col, crs=crs)
 
--- pandas_utils_pckg/setup.py
+++ pandas_utils_pckg/setup.py
@@ -46,7 +46,7 @@ def readme():
 
 setup(
     name='pandas_utils',
-    version='1.2.3',
+    version='1.2.4',
     packages=find_packages(),
     url=f'{GIT_REPO}/tree/master/pandas_utils_pckg',
     author='Ernesto Arredondo Martínez',
