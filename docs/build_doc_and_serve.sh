2ac3b58ea0c0969530d2ac4463b42a3a074b2f21
arredoer <ernesto.arredondo@portdebarcelona.cat>
2024-10-22 23:32:34 +0200

    add use of PREFIX_DOCS env var for serve pdocs

--- docs/build_doc_and_serve.sh
+++ docs/build_doc_and_serve.sh
@@ -6,8 +6,9 @@ PACKAGES_DOC=${PACKAGES_DOC:-cx_oracle_spatial extra_osgeo_utils extra_utils pan
 URL_LOGO_APB=${URL_LOGO_APB:-https://www.portdebarcelona.cat/themes/custom/portbcn/logo.svg}
 MAIN_README_DOC=${MAIN_README_DOC:-README.md}
 JINJA_TEMPLATES=${JINJA_TEMPLATES:-./templates}
+PREFIX_DOCS=${PREFIX_DOCS:-generic_python_packages}
 
-pdoc $PACKAGES_DOC -t "${JINJA_TEMPLATES}" -d google --logo ${URL_LOGO_APB} -o ./html
+pdoc $PACKAGES_DOC -t "${JINJA_TEMPLATES}" -d google --logo ${URL_LOGO_APB} -o "./html/${PREFIX_DOCS}"
 
 cp "${JINJA_TEMPLATES}/favicon.ico" ./html/
 
