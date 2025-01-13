4d0fe8b7116fd98f27290ea23dcfb59878f7db42
Ernesto Arredondo Martínez <ernesto.arredondo@portdebarcelona.cat>
2025-01-13 20:13:56 +0100

    on build script remove previous dist folder to not add previous builds

--- build_pckg.sh
+++ build_pckg.sh
@@ -7,9 +7,6 @@
 #     REPOSITORY  - the repository where the package will be uploaded (default is  pypi )
 #  The script uses the  twine  package to upload the package to the repository. The  TWINE_USERNAME  and  TWINE_PASSWORD  environment variables are set to  __token__  and the API token, respectively.
 #  The  --non-interactive  flag is used to prevent the script from asking for confirmation before uploading the package.
-#  Step 3: Create a GitHub Action
-#  Now that we have the script ready, we can create a GitHub Action to automate the process of building and uploading the package.
-#  Create a new file named  build_pckg.yml  in the  .github/workflows  directory.
 
 if [ -z "$1" ] || [ -z "$2" ]; then
   echo "Usage: $0 <PACKAGE_FOLDER> <PYPI_API_TOKEN> [repository]"
@@ -30,5 +27,7 @@ cd "$PACKAGE_FOLDER" || exit
 # Set the environment variable GIT_REPO to null to not use it
 export GIT_REPO="null"
 
+# Remove the dist folder if exists
+[ -d dist ] && rm -rf dist
 python -m build
 twine upload --username __token__ --password "$PYPI_API_TOKEN" --verbose --non-interactive -r "$REPOSITORY" dist/*
